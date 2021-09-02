fc88b3552fbede288f74a94867e482276807a182
ghiffaryr <grifqialdi@gmail.com>
2021-09-02 09:07:25 +0700

    Add belajar_java_07 and belajar_java_08

---
+++ belajar_java_08/src/com/tutorial/Main.java
@@ -0,0 +1,26 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //This is comment
+        /*
+        Paragraph comment
+         */
+        System.out.println("yow!");
+
+        int x = 10; //assignment
+        System.out.println("x value = " + x);
+
+        x = 20;
+        System.out.println("new x value = " + x);
+
+        int b; //declaration
+
+        b = 7;
+
+        System.out.println("b value = " + x);
+
+    }
+}
