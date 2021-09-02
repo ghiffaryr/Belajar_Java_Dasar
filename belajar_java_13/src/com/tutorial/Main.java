9efd46704dd94c2ccc450a19ca149bbdff58ff85
ghiffaryr <grifqialdi@gmail.com>
2021-09-02 20:55:36 +0700

    Add belajar_java_13

---
+++ belajar_java_13/src/com/tutorial/Main.java
@@ -0,0 +1,33 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //addition assignment operator
+        int a = 0;
+        a += 10; //a = a + 10
+        System.out.println("a value = " + a);
+
+        //substraction assignment operator
+        int b = 20;
+        b -= 10; //b = b - 10
+        System.out.println("b value = " + b);
+
+        //multiplication assignment operator
+        int c = 10;
+        c *= 10; //c = c * 10
+        System.out.println("c value = " + c);
+
+        //division assignment operator
+        int d = 10;
+        d /= 10; //d = d / 10
+        System.out.println("d value = " + d);
+
+        //modulus assignment operator
+        int e = 11;
+        e %= 10; //e = e mod 10
+        System.out.println("e value = " + e);
+
+    }
+}
