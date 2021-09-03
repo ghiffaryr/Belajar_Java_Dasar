f18d3bec30cc81a78bc3b56bf892b1c37f8e13d7
ghiffaryr <grifqialdi@gmail.com>
2021-09-03 17:31:06 +0700

    Update belajar_java_16

--- belajar_java_16/src/com/tutorial/Main.java
+++ belajar_java_16/src/com/tutorial/Main.java
@@ -1,2 +1,82 @@
-package com.tutorial;public class Main {
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //bitwise_operator
+
+        byte a = 3;
+        byte b,c;
+        String a_bits,b_bits,c_bits;
+
+        //shift left
+        System.out.println("Shift left (<<)");
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        b = (byte)(a << 3);
+        b_bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("%s = %d \n",b_bits, b);
+
+        //shift right
+        System.out.println("Shift right (>>)");
+        a = 24;
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        b = (byte)(a >> 2);
+        b_bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("%s = %d \n",b_bits, b);
+
+        //bitwise OR
+        System.out.println("Bitwise OR (|)");
+        a = 24;
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        b = 12;
+        b_bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("%s = %d \n",b_bits, b);
+        System.out.println("--------------------OR");
+        c = (byte)(a | b);
+        c_bits = String.format("%8s",Integer.toBinaryString(c)).replace(' ','0');
+        System.out.printf("%s = %d \n",c_bits, c);
+
+        //bitwise AND
+        System.out.println("Bitwise AND (&)");
+        a = 24;
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        b = 12;
+        b_bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("%s = %d \n",b_bits, b);
+        System.out.println("--------------------AND");
+        c = (byte)(a & b);
+        c_bits = String.format("%8s",Integer.toBinaryString(c)).replace(' ','0');
+        System.out.printf("%s = %d \n",c_bits, c);
+
+        //bitwise XOR
+        System.out.println("Bitwise XOR (^)");
+        a = 24;
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        b = 12;
+        b_bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("%s = %d \n",b_bits, b);
+        System.out.println("--------------------XOR");
+        c = (byte)(a ^ b);
+        c_bits = String.format("%8s",Integer.toBinaryString(c)).replace(' ','0');
+        System.out.printf("%s = %d \n",c_bits, c);
+
+        //bitwise not
+        System.out.println("Bitwise not (~)");
+        a = 24;
+        b = (byte) (~a);
+        a_bits = String.format("%8s",Integer.toBinaryString(a)).replace(' ','0');
+        System.out.printf("%s = %d \n",a_bits, a);
+        System.out.println("--------------------NOT");
+        b_bits = String.format("%8s",Integer.toBinaryString(b).substring(24));
+        System.out.printf("%s = %d \n",b_bits, b);
+
+
+    }
+
 }
