14cc56a1943dcabd0dfedc2c981c8aaaea27dcc0
ghiffaryr <grifqialdi@gmail.com>
2021-09-05 17:37:31 +0700

    Add belajar_java_20

---
+++ belajar_java_20/src/com/tutorial/Main.java
@@ -0,0 +1,27 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //if_statement
+
+        int a  = 6;
+        System.out.println("value = " + a);
+
+        //this is the nest
+         if (a == 5){
+
+             System.out.println("this is the true path");
+
+         } else {
+
+             System.out.println("this is the false path");
+
+         }
+
+        System.out.println("finish");
+
+    }
+
+}
