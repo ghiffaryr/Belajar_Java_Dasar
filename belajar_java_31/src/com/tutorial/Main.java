a44c5b4556450bd1dad6a822ac8fe9dd89dd32d5
ghiffaryr <grifqialdi@gmail.com>
2021-09-08 22:53:03 +0700

    Add belajar_java_31

---
+++ belajar_java_31/src/com/tutorial/Main.java
@@ -0,0 +1,29 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //calculating fibonacci no-n
+        int f_n,f_n_1,f_n_2,n;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("taking fibonacci no-:");
+        n = userInput.nextInt();
+
+        f_n_2 = 0;
+        f_n_1 = 1;
+        f_n = 1;
+
+        for(int i = 1; i <= n; i++){
+            System.out.println("value no-" + i + " is " + f_n);
+            f_n = f_n_1 + f_n_2;
+            f_n_2 = f_n_1;
+            f_n_1 = f_n;
+        }
+
+    }
+
+}
