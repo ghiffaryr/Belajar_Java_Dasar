742339e8024c99e6df0663f84015900b42192681
ghiffaryr <grifqialdi@gmail.com>
2021-09-09 10:31:44 +0700

    Add belajar_java_35

---
+++ belajar_java_35/src/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(simple());
+
+        voidFunc("whatever");
+        goodMorning("cuk");
+        goodMorning("ngab");
+        
+    }
+
+    private static void goodMorning(String name){
+        System.out.println("good morning " + name);
+    }
+
+    private static void voidFunc(String input){
+        System.out.println(input);
+    }
+    private static float simple(){
+        return 10.0f;
+    }
+}
